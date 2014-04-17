e695046ec37f15e0c39508fcc0c05ed1a402281a
barry.hughes <barry.hughes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-17 03:21:15 +0000

    1.3.3 final amendment!
    git-svn-id: https://plugins.svn.wordpress.org/event-rocket/tags/1.3.3@895518 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ launcher.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Plugin Name: Event Rocket
+ * Description: Adds shortcodes and other tools to help build sites with The Events Calendar/Events Calendar PRO.
+ * Version: 1.3.3
+ * Author: Barry Hughes
+ * Author URI: http://codingkills.me
+ * License: GPLv3 or later
+ */
+
+defined( 'ABSPATH' ) or exit();
+
+
+add_action( 'plugins_loaded', 'eventrocket_launch' );
+
+function eventrocket_launch() {
+	if ( ! class_exists( 'TribeEvents' ) || version_compare( TribeEvents::VERSION, '3.4', '<' ) ) {
+		eventrocket_abort_launch();
+		return;
+	}
+
+	define( 'EVENTROCKET_INC', dirname( __FILE__ ) . '/inc' );
+	require EVENTROCKET_INC . '/groundcontrol.php';
+}
+
+function eventrocket_abort_launch() {
+	add_action( 'admin_notices', 'eventrocket_explain_failure' );
+}
+
+function eventrocket_explain_failure() {
+	$msg =  __( 'A suitable version of The Events Calendar does not seem to be installed. Event Rocket cannot launch.', 'event-rocket' );
+	echo '<div class="error"> <p> ' . $msg . ' </p> </div>';
+}
