7fa202572c70974628a6b2b72be6156d8d651617
barry.hughes <barry.hughes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-07 19:04:27 +0000

    1.4.1 (adds event embed shortcode)
    git-svn-id: https://plugins.svn.wordpress.org/event-rocket/tags/1.4.1@909933 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ launcher.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Plugin Name: Event Rocket
+ * Description: Adds shortcodes and other tools to help build sites with The Events Calendar/Events Calendar PRO.
+ * Version: 1.4.1
+ * Author: Barry Hughes
+ * Author URI: http://codingkills.me
+ * License: GPLv3 or later
+ */
+
+defined( 'ABSPATH' ) or exit();
+
+
+add_action( 'plugins_loaded', 'eventrocket_launch' );
+
+function eventrocket_launch() {
+	define( 'EVENTROCKET_INC', dirname( __FILE__ ) . '/inc' );
+	define( 'EVENTROCKET_URL', plugin_dir_url( __FILE__ ) );
+
+	if ( ! class_exists( 'TribeEvents' ) || version_compare( TribeEvents::VERSION, '3.4', '<' ) ) {
+		eventrocket_abort_launch();
+		return;
+	}
+
+	require EVENTROCKET_INC . '/groundcontrol.php';
+}
+
+function eventrocket_abort_launch() {
+	global $pagenow;
+
+	require_once EVENTROCKET_INC . '/jettison.php';
+	if ( 'plugins.php' === $pagenow ) add_action( 'admin_notices', 'eventrocket_explain_failure' );
+}
+
+function eventrocket_explain_failure() {
+	$msg =  __( 'Event Rocket requires a suitable version of The Events Calendar to be activated in order to provide '
+		. 'full functionality (data cleanup tools will still be available, though).', 'eventrocket' );
+	echo '<div class="error"> <p> ' . $msg . ' </p> </div>';
+}
