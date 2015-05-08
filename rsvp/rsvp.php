f3dd8c2993f7cdcbadba8c1b407d7fe292159992
barry.hughes <barry.hughes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-08 09:45:52 +0000

    Add 2.5.1 (cleanup tools fix)
    git-svn-id: https://plugins.svn.wordpress.org/event-rocket/tags/2.5.1@1156002 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ rsvp/rsvp.php
@@ -0,0 +1,9 @@
+<?php
+defined( 'ABSPATH' ) or die();
+
+require_once EVENTROCKET_INC . '/rsvp/attendance.php';
+require_once EVENTROCKET_INC . '/rsvp/attendee-list.php';
+require_once EVENTROCKET_INC . '/rsvp/form.php';
+require_once EVENTROCKET_INC . '/rsvp/manager.php';
+
+eventrocket_rsvp();
